22bd48266ebd6e747e63bbd8d4f3401ccf36b270
Sachielsc <sachielsc@gmail.com>
2017-11-06 16:18:19 +1300

    add models and viewmodels

---
+++ CharlesMyHousing/Models/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CharlesMyHousing.Models
+{
+    public class SearchResult
+    {
+    }
+}
---
+++ CharlesMyHousing/ViewModels/SearchView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CharlesMyHousing.Models;
+using PagedList;
+
+namespace CharlesMyHousing.ViewModels
+{
+    public class SearchView
+    {
+        public virtual SearchCondition SearchCondition{ set; get; }
+        public virtual IPagedList<SearchView> SearchViews { set; get; }
+    }
+}
