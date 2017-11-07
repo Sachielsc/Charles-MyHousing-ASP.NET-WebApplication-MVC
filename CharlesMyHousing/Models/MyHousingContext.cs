5d027094e7155f4c008045354bdd8f08a9000fe8
Sachielsc <sachielsc@gmail.com>
2017-11-08 12:14:09 +1300

    start working on search view pages

--- CharlesMyHousing/Models/MyHousingContext.cs
+++ CharlesMyHousing/Models/MyHousingContext.cs
@@ -18,10 +18,6 @@ namespace CharlesMyHousing.Models
         public DbSet<HouseState> HouseStates { get; set; }
         public DbSet<RentCase> RentCases { get; set; }
         public DbSet<HousePicture> HousePictures { get; set; }
-        //public DbSet<SearchCondition> SearchConditions { get; set; }
-        //public DbSet<SearchResult> SearchResults { get; set; }
-        //public DbSet<DetailView> DetaukDetailViews { get; set; }
-        //public DbSet<SearchView> SearchViews { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
